4da8d878aa6cb6e92cfad13423f44c4b962aff19
shayanTaki <takishayan@gmail.com>
2023-08-24 06:19:30 -0700

    END comit for PlanB

--- NewWeb/Controllers/HomeController.cs
+++ NewWeb/Controllers/HomeController.cs
@@ -671,6 +671,20 @@ namespace NewWeb.Controllers
 
                 return View("SearchKoliPost22");
             }
+            //###############################################################################################################
+
+            else if (string.IsNullOrEmpty(Type) && string.IsNullOrEmpty(MainLine) && string.IsNullOrEmpty(EnterBIW) && string.IsNullOrEmpty(BIW)
+                && string.IsNullOrEmpty(MetalFinish) && string.IsNullOrEmpty(EnterPaint) && string.IsNullOrEmpty(Primer) && string.IsNullOrEmpty(TopCoat) && string.IsNullOrEmpty(ExitPaint) && string.IsNullOrEmpty(EnterTrim)
+                && string.IsNullOrEmpty(ExitChassis) && string.IsNullOrEmpty(FinalTrim) && !string.IsNullOrEmpty(ExitTrim))
+            {
+
+                ExitTrim = ExitTrim.Replace("\t", "");
+                var vehicleList = PlanningDBcontax.Tblvehicle.Where(u =>
+                u.ExitTrim == ExitTrim).ToList();
+                ViewBag.vehicleList = vehicleList;
+
+                return View("SearchKoliPost23");
+            }
             else
             {
                 return View();
