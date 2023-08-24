d6573726dce9516ea0c634f8f195d274d9fb186f
shayanTaki <takishayan@gmail.com>
2023-08-24 06:02:58 -0700

    Sone6

--- NewWeb/Controllers/HomeController.cs
+++ NewWeb/Controllers/HomeController.cs
@@ -573,6 +573,20 @@ namespace NewWeb.Controllers
 
                 return View("SearchKoliPost15");
             }
+            //###############################################################################################################
+
+            else if (string.IsNullOrEmpty(Type) && string.IsNullOrEmpty(MainLine) && string.IsNullOrEmpty(EnterBIW) && string.IsNullOrEmpty(BIW)
+                && string.IsNullOrEmpty(MetalFinish) && !string.IsNullOrEmpty(EnterPaint) && string.IsNullOrEmpty(Primer) && string.IsNullOrEmpty(TopCoat) && string.IsNullOrEmpty(ExitPaint) && string.IsNullOrEmpty(EnterTrim)
+                && string.IsNullOrEmpty(ExitChassis) && string.IsNullOrEmpty(FinalTrim) && string.IsNullOrEmpty(ExitTrim))
+            {
+
+                EnterPaint = EnterPaint.Replace("\t", "");
+                var vehicleList = PlanningDBcontax.Tblvehicle.Where(u =>
+                u.EnterPaint == EnterPaint).ToList();
+                ViewBag.vehicleList = vehicleList;
+
+                return View("SearchKoliPost16");
+            }
             else
             {
                 return View();
