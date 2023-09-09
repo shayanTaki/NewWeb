77a4e3934c5fbe693efc9a2888161eacb608b866
shayanTaki <takishayan@gmail.com>
2023-09-09 16:30:28 -0700

    Kakooooooo

--- NewWeb/Controllers/NewHomeController.cs
+++ NewWeb/Controllers/NewHomeController.cs
@@ -19,57 +19,186 @@ namespace NewWeb.Controllers
             this.ContaxDBlogin = ContaxDBlogin;
             this.PlanningDBcontax = PlanningDBcontax;
         }
-
+        
         public IActionResult Index()
         {
             return View();
         }
 
+
+
         [HttpPost]
         public ActionResult ProcessFormData()
         {
-            // دریافت داده‌های فرم از درخواست POST
-            bool checkbox1 = Convert.ToBoolean(Request.Form["checkbox1"]);
-            bool checkbox2 = Convert.ToBoolean(Request.Form["checkbox2"]);
-            //bool checkbox3 = Convert.ToBoolean(Request.Form["checkbox3"]);
-            //bool checkbox1 = Convert.ToBoolean(Request.Form["checkbox1"]);
-            //bool checkbox2 = Convert.ToBoolean(Request.Form["checkbox2"]);
-            //bool checkbox3 = Convert.ToBoolean(Request.Form["checkbox3"]);
-            //bool checkbox1 = Convert.ToBoolean(Request.Form["checkbox1"]);
-            //bool checkbox2 = Convert.ToBoolean(Request.Form["checkbox2"]);
-            //bool checkbox3 = Convert.ToBoolean(Request.Form["checkbox3"]);
-            //bool checkbox1 = Convert.ToBoolean(Request.Form["checkbox1"]);
-            //bool checkbox2 = Convert.ToBoolean(Request.Form["checkbox2"]);
-            //bool checkbox3 = Convert.ToBoolean(Request.Form["checkbox3"]);
-            //bool checkbox1 = Convert.ToBoolean(Request.Form["checkbox1"]);
-            //bool checkbox2 = Convert.ToBoolean(Request.Form["checkbox2"]);
-            //bool checkbox3 = Convert.ToBoolean(Request.Form["checkbox3"]);
-            //bool checkbox3 = Convert.ToBoolean(Request.Form["checkbox3"]);
+            ////دریافت داده‌های فرم از درخواست POST
+            ////bool Vin = Convert.ToBoolean(Request.Form["Vin"]);
+            ////bool Type = Convert.ToBoolean(Request.Form["Type"]);
+            ////bool MetalFinish = Convert.ToBoolean(Request.Form["MainLine"]);
+            ////bool TopCoat = Convert.ToBoolean(Request.Form["enterBIW"]);
+            ////bool ExitChassis = Convert.ToBoolean(Request.Form["ExitChassis"]);
+            ////bool FinalTrim = Convert.ToBoolean(Request.Form["FinalTrim"]);
+            ////bool Trans = Convert.ToBoolean(Request.Form["Trans"]);
+            ////bool checkbox2 = Convert.ToBoolean(Request.Form["checkbox2"]);
+            ////bool checkbox3 = Convert.ToBoolean(Request.Form["checkbox3"]);
+            ////bool checkbox1 = Convert.ToBoolean(Request.Form["checkbox1"]);
+            ////bool checkbox2 = Convert.ToBoolean(Request.Form["checkbox2"]);
+            ////bool checkbox3 = Convert.ToBoolean(Request.Form["checkbox3"]);
+            ////bool checkbox1 = Convert.ToBoolean(Request.Form["checkbox1"]);
+            ////bool checkbox2 = Convert.ToBoolean(Request.Form["checkbox2"]);
+            ////bool checkbox3 = Convert.ToBoolean(Request.Form["checkbox3"]);
+            ////bool checkbox3 = Convert.ToBoolean(Request.Form["checkbox3"]);
+
+            string VinData = Request.Form["VinData"];
+            string TypeData = Request.Form["TypeData"];
+            string MetalFinishData = Request.Form["MainLineData"];
+            string TopCoatData = Request.Form["enterBIWdata"];
+            string ExitChassisData = Request.Form["ExitChassisData"];
+            string FinalTrimData = Request.Form["FinalTrimData"];
+            string TransData = Request.Form["TransData"];
+
+
+
+            // ایجاد یک دیکشنری برای نگهداری نام متغیرها و مقادیر بولین آنها
+            Dictionary<string, bool> formValues = new Dictionary<string, bool>();
+
+            // نمونه اضافه کردن مقادیر به دیکشنری
+            formValues["Vin"] = Convert.ToBoolean(Request.Form["Vin"]);
+            formValues["Type"] = Convert.ToBoolean(Request.Form["Type"]);
+            formValues["MetalFinish"] = Convert.ToBoolean(Request.Form["MainLine"]);
+            formValues["TopCoat"] = Convert.ToBoolean(Request.Form["enterBIW"]);
+            formValues["ExitChassis"] = Convert.ToBoolean(Request.Form["ExitChassis"]);
+            formValues["FinalTrim"] = Convert.ToBoolean(Request.Form["FinalTrim"]);
+            formValues["Trans"] = Convert.ToBoolean(Request.Form["Trans"]);
 
 
 
 
-            var vehicleList = PlanningDBcontax.Tblvehicle.Where(u => !checkbox1 && u.Ok4St == checkbox1).ToArray();
+
+
+            var vin1 = PlanningDBcontax.Tblvehicle.Where(x => x.vin == VinData).ToArray();
+            var TypeN = PlanningDBcontax.Tblvehicle.Where(x => x.vin == VinData).ToArray();
+            var metalN = PlanningDBcontax.Tblvehicle.Where(x => x.vin == VinData).ToArray();
+            var ExitN = PlanningDBcontax.Tblvehicle.Where(x => x.vin == VinData).ToArray();
+            var FinalN = PlanningDBcontax.Tblvehicle.Where(x => x.vin == VinData).ToArray();
+            var TransN = PlanningDBcontax.Tblvehicle.Where(x => x.vin == VinData).ToArray();
 
 
 
-            string date1 = Request.Form["date1"];
+
+
+
+            // حلقه foreach برای جداگانه بررسی متغیرها
+            foreach (var kvp in formValues)
+            {
+                string variableName = kvp.Key;
+                bool variableValue = kvp.Value;
+
+                // اجرای عملیات مورد نظر بر اساس نام متغیر و مقدار بولین آن
+                if (variableValue)
+                {
+                   if (variableName == "Vin")
+                    {
+                         var vin2 = PlanningDBcontax.Tblvehicle.Where(x => x.vin == VinData).ToArray();
+                          vin1 = vin2;
+                        break;
+                    }
+                   else if(variableName == "Type")
+                    {
+                        var TypeBank = PlanningDBcontax.Tblvehicle.Where(x => x.Type == TypeData).ToArray();
+                        TypeN = TypeBank;
+                        continue;
+                    }
+                    else if (variableName == "MetalFinish")
+                    {
+                        var MetalFinishBank = PlanningDBcontax.Tblvehicle.Where(x => x.Type == TypeData).ToArray();
+                        metalN = MetalFinishBank;
+                        continue;
+                    }
+                    else if (variableName == "ExitChassis")
+                    {
+                        var ExitChassisBank = PlanningDBcontax.Tblvehicle.Where(x => x.Type == TypeData).ToArray();
+                        ExitN = ExitChassisBank;
+                        continue;
+                    }
+                    else if (variableName == "FinalTrim")
+                    {
+                        var FinalTrimBank = PlanningDBcontax.Tblvehicle.Where(x => x.Type == TypeData).ToArray();
+                        FinalN = FinalTrimBank;
+                        continue;
+                    }
+                    else if (variableName == "Trans")
+                    {
+                        var TransBank = PlanningDBcontax.Tblvehicle.Where(x => x.Type == TypeData).ToArray();
+                        TransN = TransBank;
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+
             
-           
-            
-            string date2 = Request.Form["date2"];
-            string date3 = Request.Form["date3"];
 
+
+
+            if (TypeN != null)
+            {
+                var TypeO = TypeN;
+            }
+            else if(metalN != null)
+            {
+                var metalO = metalN;
+            }
+            else if(ex)
             // انجام عملیات پردازش بر روی داده‌های دریافتی
             // در اینجا می‌توانید با داده‌ها عملیات مورد نظر خود را انجام دهید
-
-            return Content("عملیات با موفقیت انجام شد");
+            ViewBag.vehicleList = vin1;
+            return View("gozaresh_filterPost");
         }
 
         public IActionResult gozaresh_filter()
         {
             return View();
         }
+        //public IActionResult NewSearch()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public IActionResult NewSearchPost()
+        //{
+        //    bool virtualTypeBox = Convert.ToBoolean(Request.Form["virtualTypeBox"]);
+        //    bool metalFinishBox = Convert.ToBoolean(Request.Form["metalFinishBox"]);
+        //    bool TopcoatBox = Convert.ToBoolean(Request.Form["TopcoatBox"]);
 
+
+           
+
+        //    string virtualTypeBoxValue1 = Request.Form["virtualTypeBoxValue"];
+        //    string metalFinishDate1 = Request.Form["metalFinishDate"];
+        //    string TopcoatDate1 = Request.Form["TopcoatDate"];
+
+        //    var vin2 = PlanningDBcontax.Tblvehicle.Where(x => virtualTypeBox && x.VirtualType == virtualTypeBoxValue1 || metalFinishBox && x.MetalFinish == metalFinishDate1 || TopcoatBox && x.TopCoat == TopcoatDate1).ToArray();
+        //    ViewBag.vehicleList1 = vin2;
+        //    return View();
+        //}
+
+
+        [HttpPost]
+        public IActionResult gozaresh_filterPost()
+        {
+            return View();
+        }
+
+
+        
     }
 }
---
+++ NewWeb/wwwroot/js/js2.js
@@ -0,0 +1,154 @@
+﻿const ul = document.querySelector("ul div"),
+    inputbox = document.querySelector("input");
+
+const Button = document.querySelector("button");
+let tags = [];
+var virtualTypesObj = [
+    { value: "752N", type: "J4-CVT", name: "jacj4" },
+    { value: "742S", type: "S3-CVT", name: "jacs3" },
+    { value: "762S", type: "S5 1.5-DCT", name: "jacs5" },
+    { value: "811P", type: "T8-MT", name: "kmct8" },
+    { value: "822S", type: "K7-DCT", name: "kmck7" },
+    { value: "832N", type: "J7-DCT", name: "kmcj7" },
+    { value: "842N", type: "A5-AT", name: "kmca5" },
+    { value: "852S", type: "X5-AT", name: "kmcx5" },
+];
+var BodyShopObj = ["metalfinish", "Metalfinish", "METALFINISH", "MetalFinish", "بدنه", "سالنبدنه"];
+var PaintShopObj = ["topcoat", "TopCoat", "Topcoat", "رنگ", "رنگشده", "سالنرنگ"];
+// var virtualTypesNames = virtualTypesObj.map(elements => {
+//     return elements.name
+// });
+
+var virtualTypesMap = new Map();
+virtualTypesMap.set("752N", "jacj4");
+virtualTypesMap.set("742S", "jacs3");
+virtualTypesMap.set("762S", "jacs5");
+virtualTypesMap.set("811P", "kmct8");
+virtualTypesMap.set("822S", "kmck7");
+virtualTypesMap.set("832N", "kmcj7");
+virtualTypesMap.set("842N", "kmca5");
+virtualTypesMap.set("852S", "kmcx5");
+
+var vitualtypeRes;
+
+function createTag() {
+    ul.querySelectorAll("li").forEach(li => li.remove(),);
+    tags.forEach(tag => {
+        let Litag = `<li class="defualt">${tag}<img src="assets/images/xIcon.svg" onclick="remove(this, '${tag}')" alt=""></li>`;
+        ul.insertAdjacentHTML("afterbegin", Litag);
+    });
+}
+
+function remove(element, tag) {
+    let index = tags.indexOf(tag);
+    tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+    element.parentElement.remove();
+    // document.getElementsByName("virtualTypeBoxValue")[0].value = "";
+    // document.getElementById("virtualTypeBox").checked = false;
+    console.log(tags)
+}
+function addList(e) {
+    if (e.key == "Enter") {
+        let tag = e.target.value.replace(/\s/g, '');
+        if (tag.length > 1 && !tags.includes(tag)) {
+            tag.split('-').forEach(tag => {
+                tags.push(tag);
+                console.log(tags);
+                createTag();
+            });
+            inputbox.addEventListener('Enter', inputbox.value = "");
+        }
+    }
+}
+inputbox.addEventListener('keyup', addList);
+
+// recongnizing difrenet values 
+const Fields = [];
+
+function findingVIrtualtype() {
+    findingMetalFinish();
+    findingTopCoat();
+    // var res = virtualTypesNames.find(v => v == tags);
+    var CheckVirtualtype = tags.filter(el => virtualTypesObj.map(elements => {
+        return elements.name
+    }).includes(el));
+    var indexOf = virtualTypesObj.findIndex(object => {
+        return object.name == CheckVirtualtype;
+    });
+    vitualtypeRes = virtualTypesObj[indexOf].value;
+
+    console.log(CheckVirtualtype ? CheckVirtualtype : "wasn't found");
+    console.log(vitualtypeRes);
+
+    if (vitualtypeRes.length > 0) {
+        document.getElementsByName("virtualTypeBoxValue")[0].value = "";
+        document.getElementsByName("virtualTypeBoxValue")[0].value = vitualtypeRes;
+        document.getElementById("virtualTypeBox").checked = true;
+    };
+
+
+}
+
+function findingMetalFinish() {
+    var CheckMetalFinish = tags.filter(function (e) {
+        return BodyShopObj.includes(e);
+    });
+    if (CheckMetalFinish.length > 0) {
+        // document.getElementsByName("virtualTypeBoxValue")[0].value = "";
+        document.getElementsByName("metalFinishDate")[0].value = "1402/03/28";
+        document.getElementById("metalFinishBox").checked = true;
+
+    }
+}
+
+
+function findingTopCoat() {
+    var CheckTopCoat = tags.filter(function (e) {
+        return PaintShopObj.includes(e);
+    });
+    if (CheckTopCoat.length > 0) {
+        // document.getElementsByName("virtualTypeBoxValue")[0].value = "";
+        document.getElementsByName("TopcoatDate")[0].value = "1402/03/28";
+        document.getElementById("TopcoatBox").checked = true;
+
+    }
+}
+
+
+// function to pass the data to server
+function submitForm() {
+    var formData = {
+        virtualTypeBox: document.getElementById("virtualTypeBox").checked,
+        metalFinishBox: document.getElementById("metalFinishBox").checked,
+        TopcoatBox: document.getElementById("TopcoatBox").checked,
+
+        virtualTypeBoxValue: document.getElementsByName("virtualTypeBoxValue")[0].value,
+        metalFinishDate: document.getElementsByName("metalFinishDate")[0].value,
+        TopcoatDate: document.getElementsByName("TopcoatDate")[0].value,
+    };
+
+
+
+
+    // ایجاد یک فرم مخفی برای ارسال داده‌ها به سمت سرور
+    var hiddenForm = document.createElement("form");
+    hiddenForm.method = "post";
+    hiddenForm.action = "/NewHome/NewSearchPost"; // مسیر مورد نظر برای درخواست POST
+
+    // ایجاد یک المان input برای هر فیلد داده
+    for (var key in formData) {
+        var input = document.createElement("input");
+        input.type = "hidden"; // نوع المان input
+        input.name = key; // نام فیلد داده
+        input.value = formData[key]; // مقدار فیلد داده
+
+        // اضافه کردن المان input به فرم مخفی
+        hiddenForm.appendChild(input);
+    }
+
+    // اضافه کردن فرم مخفی به صفحه
+    document.body.appendChild(hiddenForm);
+
+    // ارسال فرم مخفی
+    hiddenForm.submit();
+}
